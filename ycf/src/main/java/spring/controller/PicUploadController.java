f427a5a9f3331859c00e701542fbab4730792deb
86157 <838888921@qq.com>
2019-12-23 09:38:33 +0800

    。

--- ycf/src/main/java/spring/controller/PicUploadController.java
+++ ycf/src/main/java/spring/controller/PicUploadController.java
@@ -7,15 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
-import org.springframework.web.servlet.ModelAndView;
 import spring.dto.BaseCommonResult;
 import spring.dto.request.UserFileUploadRequest;
 import spring.service.AliYunService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 
 @Api(tags = "图片上传",basePath = "/picCenter/pic")
@@ -35,7 +32,7 @@ public class PicUploadController {
 
     @RequestMapping(value = "/weChat/uploadImage", method = { RequestMethod.POST,RequestMethod.GET})
     @ResponseBody
-    public BaseCommonResult uploadImage(HttpServletRequest request, HttpServletResponse response) throws IOException, ExecutionException, InterruptedException {
+    public BaseCommonResult uploadImage(HttpServletRequest request, HttpServletResponse response) throws  ExecutionException, InterruptedException {
         log.info("图片上传开始");
         MultipartHttpServletRequest req =(MultipartHttpServletRequest)request;
         log.info("图片上传开始:{}",req);
